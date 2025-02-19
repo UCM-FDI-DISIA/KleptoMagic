23967c169476fc02e27302f5e57dfe195d8f3ce3
Zhuokai Zhu <zhuzhu@ucm.es>
2025-02-19 17:34:51 +0100

    Add empty MenuState

---
+++ KleptoMagic_project/src/Class/MenuState.cpp
@@ -0,0 +1,16 @@
+#include "MenuState.h"
+
+/*MenuState::MenuState(Game* game) : GameState(game) {
+    screenCenter = game->getWindowWidth() / 2;
+}
+
+Button* MenuState::createButton(float x, float y, int w, int h, Game::TextureName textureName) {
+    Point2D buttonPosition = {x, y};
+    Button* newButton = new Button(this, game->getTexture(textureName), buttonPosition, w, h);
+    addGameObjects(newButton);
+    addEventHandler(newButton);
+    return newButton;
+}
+
+MenuState::~MenuState() {
+}*/
---
+++ KleptoMagic_project/src/Class/MenuState.h
@@ -0,0 +1,19 @@
+#pragma once
+#ifndef MENU_STATE
+#define MENU_STATE
+
+#include "GameState.h"
+//#include "Button.h"
+
+class MenuState : public GameState {
+protected:
+    float screenCenter;
+
+public:
+    //MenuState(Game* game);
+    ~MenuState();
+
+    //Button* createButton(float x, float y, int w, int h, Game::TextureName textureName);
+};
+
+#endif // MENU_STATE
