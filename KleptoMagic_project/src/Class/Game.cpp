adc97ee49ddd2d2557fe4a80efa496894f679f28
SergioC242 <scroche@ucm.es>
2025-02-19 17:29:17 +0100

    Game añadido

---
+++ KleptoMagic_project/src/Class/Game.cpp
@@ -0,0 +1,144 @@
+﻿#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include "Game.h"
+
+using namespace std;
+
+// Formato de la especificación de una textura
+struct TextureSpec
+{
+	const char* name;	// Ruta del archivo
+	uint numColumns;	// Número de frames por fila
+	uint numRows;		// Número de frames por columna
+};
+
+// Directorio raíz de los archivos de textura
+const string textureRoot = "../assets/images/";
+
+// Especificación de las texturas del juego
+const array<TextureSpec, Game::NUM_TEXTURES> textureSpec{
+	//TextureSpec{"background.png", 9, 7},
+};
+
+Game::Game(int worldN) : exit(false) {
+
+	// Inicializa la SDL
+	SDL_Init(SDL_INIT_EVERYTHING);
+	window = SDL_CreateWindow("First test with SDL",
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		WIN_WIDTH,
+		WIN_HEIGHT,
+		SDL_WINDOW_SHOWN);
+
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+	if (window == nullptr || renderer == nullptr)
+		throw "Error cargando SDL"s;
+
+	// Carga las texturas
+	//for (int i = 0; i < NUM_TEXTURES; ++i)
+	//	textures[i] = new Texture(renderer,
+	//		(textureRoot + textureSpec[i].name).c_str(),
+	//		textureSpec[i].numRows,
+	//		textureSpec[i].numColumns);
+
+	// Creación de playstates
+	//playstate = new PlayState(worldN, this); //se fue a su metodo propio
+	
+	//mainmenu = new MainMenuState(this);
+	//GameStateMachine::pushState(mainmenu);
+	//GameStateMachine::pushState(playstate);
+}
+
+
+
+void
+Game::run()
+{
+	//delete prevWorld;
+
+	// Bucle principal del juego
+
+	while (!exit) {
+		// Marca de tiempo del inicio de la iteración
+		uint32_t inicio = SDL_GetTicks();
+
+		GameStateMachine::update();
+		GameStateMachine::render();
+
+		SDL_Event evento;
+
+		while (SDL_PollEvent(&evento)) {
+			if (evento.type == SDL_QUIT)
+				exit = true;
+			else {
+				//GameStateMachine::handleEvent(evento);
+			}
+		}
+		
+		//playstate->update();       // Actualiza el estado de los objetos del juego
+		//playstate->render();       // Dibuja los objetos en la venta
+		//playstate->handleEvents(); // Maneja los eventos de la SDL (por defecto estaba después de render?)
+
+		// Tiempo que se ha tardado en ejecutar lo anterior
+		uint32_t elapsed = SDL_GetTicks() - inicio;
+
+		// Duerme el resto de la duraci󮠤el frame
+		if (elapsed < Game::FRAME_RATE)
+			SDL_Delay(Game::FRAME_RATE - elapsed);
+	}
+}
+
+
+//void Game::statePlay(int w) {
+//	playstate = new PlayState(w, this);
+//	GameStateMachine::replaceState(playstate);
+//}
+//
+//void Game::statePause(){
+//	pausestate = new PauseState(this);
+//	GameStateMachine::pushState(pausestate);
+//}
+//
+//void Game::stateNotPause(){
+//	GameStateMachine::popState();
+//}
+//
+//void Game::stateMainMenu() {
+//	GameStateMachine::popState();
+//	mainmenu = new MainMenuState(this); //se puede comentar y ver si va
+//	GameStateMachine::pushState(mainmenu);
+//}
+//
+//void Game::stateAnimation(function<bool()> funcAnim) {
+//	animationstate = new AnimationState(this, playstate, funcAnim);
+//	GameStateMachine::pushState(animationstate);
+//}
+//
+//void Game::stateAnimationEnd() {
+//	GameStateMachine::popState();
+//	playstate->map_reload();
+//}
+//void Game::stateEnd() {
+//	endstate = new EndState(this);
+//	GameStateMachine::pushState(endstate);
+//}
+//
+//void Game::gameExit(){
+//	exit = true;
+//}
+//
+//Game::~Game()
+//{
+//	// Elimina las texturas
+//	for (Texture* texture : textures) delete texture;
+//
+//	// Desactiva la SDL
+//	SDL_DestroyRenderer(renderer);
+//	SDL_DestroyWindow(window);
+//	SDL_Quit();
+//}
---
+++ KleptoMagic_project/src/Class/Game.h
@@ -0,0 +1,78 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <array>
+#include <vector>
+#include <SDL.h>
+//#include "checkML.h"
+//
+//#include "Texture.h"
+//#include "GameObject.h"
+//#include "SceneObject.h"
+//#include "TileMap.h"
+//#include "Player.h"
+//#include "gameList.h"
+//#include "PlayState.h"
+//#include "MainMenuState.h"
+//#include "PauseState.h"
+//#include "AnimationState.h"
+//#include "EndState.h"
+#include "gameStateMachine.h"
+
+#pragma once
+
+using uint = unsigned int;
+
+class TileMap;
+class Mushroom;
+class Shell;
+class Coin;
+
+class Game : private GameStateMachine
+{
+public:
+	// Identificadores de las texturas
+	enum TextureName {
+		NUM_TEXTURES
+	};
+	// Constante globales
+	static constexpr uint WIN_WIDTH = 544;
+	static constexpr uint WIN_HEIGHT = 480;
+	static constexpr uint FRAME_RATE = 60;
+	static constexpr uint TILE_SIZE = 32;
+
+	// Ventana de la SDL (se destruir� en el destructor)
+	SDL_Window* window = nullptr;
+	// Renderizador de la SDL (para dibujar)
+	SDL_Renderer* renderer = nullptr;
+
+	// Gamestates
+	//PlayState* playstate;
+	//MainMenuState* mainmenu;
+	//PauseState* pausestate;
+	//AnimationState* animationstate;
+	//EndState* endstate;
+
+private:
+	// Array con todas las texturas del juego
+	//std::array<Texture*, NUM_TEXTURES> textures;
+	// Interruptor para terminar el juego
+	bool exit;
+
+public:
+	//Texture* getTexture(TextureName name) const { return textures[name]; }
+
+	Game(int worldN);
+	~Game();
+	void run();
+	void statePlay(int w);
+	void statePause();
+	void stateNotPause();
+	void stateMainMenu();
+	//void stateAnimation(function<bool()> funcAnim);
+	void stateAnimationEnd();
+	void stateEnd();
+	void gameExit();
+};
+
+#endif
