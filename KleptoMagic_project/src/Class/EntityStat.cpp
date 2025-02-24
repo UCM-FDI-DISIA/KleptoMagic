384b83dc7f7e12a0e8754b2cf5e400f31afad72d
SergioYago <157816685+SergioYago@users.noreply.github.com>
2025-02-24 15:10:23 +0100

    Comentarios

--- KleptoMagic_project/src/Class/EntityStat.cpp
+++ KleptoMagic_project/src/Class/EntityStat.cpp
@@ -1,7 +1,12 @@
 #include "EntityStat.h"
 EntityStat::EntityStat(float initial)
 {
+	
 	iniStat = initial;
+	endStat = initial;
+	//se setean a su minimo
+	multi = 1;
+	addi = 0;
 }
 
 EntityStat::~EntityStat()
@@ -15,12 +20,14 @@ void EntityStat::Reset()
 
 void EntityStat::ChangeAddi(float add)
 {
+	//se suma al addi el dd y se resetea
 	addi += add;
 	Reset();
 }
 
 void EntityStat::ChangeMulti(float mult)
 {
+	//se suma el nuevo multiplicador y luego se resetea
 	multi += mult;
 	Reset();
 }
