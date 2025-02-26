54a75d0f52a89540e16d230955d1fa78428f937e
DreayGD9 <dreaygd9@gmail.com>
2025-02-26 16:52:23 +0100

    Separación de datos de la sala desde el nombre del archivo
    Tamaño y salidas de la sala

--- KleptoMagic_project/src/ProceduralPrototype/DungeonRoom.cpp
+++ KleptoMagic_project/src/ProceduralPrototype/DungeonRoom.cpp
@@ -1,8 +1,22 @@
 #include "DungeonRoom.h"
+#include <iostream>
+#include <fstream>
+
+#include "./../utils/StringUtils.h"
 
 DungeonRoom::DungeonRoom(string filename)
 {
-	cout << "Creating room: " << filename << endl;
+	cout << "CREATING ROOM: " << filename << endl;
+
+	vector<string> filename_string_separated = splitStringByDelimiter(filename, "\\");
+	string actual_filename = filename_string_separated[filename_string_separated.size() - 1];
+	vector<string> room_data_from_filename = splitStringByDelimiter(actual_filename, "_");
+	cout << "Room's exits: " << room_data_from_filename[0] << endl;
+	cout << "Room's height: " << room_data_from_filename[1] << endl;
+	cout << "Room's width: " << room_data_from_filename[2] << endl;
+	cout << "Room's name: " << room_data_from_filename[3] << endl;
+	cout << endl << endl;
+	// ifstream roomFile(filename);
 }
 
 DungeonRoom::~DungeonRoom()
