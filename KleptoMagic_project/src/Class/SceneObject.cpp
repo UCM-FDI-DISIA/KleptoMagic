207ee4cfe858fe52cf74a0fdd0cdb968e6fefc0d
SergioYago <157816685+SergioYago@users.noreply.github.com>
2025-02-26 16:24:30 +0100

    SceneObject updated

--- KleptoMagic_project/src/Class/SceneObject.cpp
+++ KleptoMagic_project/src/Class/SceneObject.cpp
@@ -1,8 +1,9 @@
 #include "SceneObject.h"
 
-SceneObject::SceneObject(Vector2D ini)
+SceneObject::SceneObject(Vector2D ini,PlayState*pointer)
 {
 	position = ini;
+	playstate = pointer;
 }
 
 SceneObject::~SceneObject()
