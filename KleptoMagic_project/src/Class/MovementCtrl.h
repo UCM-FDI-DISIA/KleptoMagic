d91ee1087d50c0c1d7dbc666cd6c296bcdd1fe34
MauroUCM <maumar03@ucm.es>
2025-03-10 16:01:21 +0100

    transform añadido al movement

--- KleptoMagic_project/src/Class/MovementCtrl.h
+++ KleptoMagic_project/src/Class/MovementCtrl.h
@@ -1,12 +1,14 @@
 #pragma once
 #include "../utils/Vector2D.h"
 #include "../ecs/Component.h"
+#include "Transform.h"
+
 class MovementCtrl : public ecs::Component {
 public:
 	__CMPID_DECL__(ecs::cmp::MOVEMENTCTRL);
 
 	void initComponent() override; //para coger el Vector2D / transform si se cambia a eso
-	void Move(Vector2D& pos, Vector2D& movement, float speed);
+	void Move(Transform& transform, Vector2D movementVector, float speed);
 
 
 private:
