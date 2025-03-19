023d526fd4e5e005ca4770cffe8fc8140bd258cc
Zhuokai Zhu <zhuzhu@ucm.es>
2025-03-19 13:48:12 +0100

    Cambiar clase button a ecs (falta las clases de menus, para poder compilar)

--- KleptoMagic_project/src/Class/Button.h
+++ KleptoMagic_project/src/Class/Button.h
@@ -2,31 +2,33 @@
 #ifndef BUTTON_H
 #define BUTTON_H
 
-#include "GameObject.h"
-#include <functional>
+#include "../ecs/ecs.h"
+#include "../ecs/ecs_defs.h"
+#include "../ecs/Component.h"
+#include "EventHandler.h"
+#include "../sdlutils/InputHandler.h"
+#include "InputManager.h"
 #include "../utils/Vector2D.h"
-#include <vector>
-#include "GameState.h"
-#include "Game.h" 
+#include <functional>
+#include <SDL.h>
 using OnClickCallback = std::function<void()>;
 
-class Button : public GameObject, public EventHandler {
-private:
-	std::vector<OnClickCallback> callbacks;
-	SDL_Rect buttonBox;
-	Vector2D position;
-	bool isHovered;
-	int width, height;
-
+class Button : public ecs::Component, public EventHandler {
 public:
-	Button(GameState* state, Texture* tex, Vector2D pos, int w, int h);
-	GameState* getState() const { return state; };
-	void setButtonBox(int x, int y, int w, int h);
-	void addCallback(OnClickCallback callback);
-	void handleEvent(const SDL_Event& event);
-	void render() const override;
-	void update() override;
-	void emit() const;
+    __CMPID_DECL__(ecs::cmp::GAMECTRL)
+
+        Button(std::function<void()> onClick, Vector2D position, Vector2D size);
+    void initComponent() override;
+    void update() override;
+    void render() override;
+    void handleEvent(const SDL_Event& event) override;
+
+private:
+    bool isInside(int x, int y) const;
+    std::function<void()> _onClick;
+    Vector2D _position;
+    Vector2D _size;
+    InputHandler* _inputHandler;
 };
 
 #endif // BUTTON_H
