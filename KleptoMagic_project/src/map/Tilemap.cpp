3cc64f54195332720127471ec25513ca03410fc9
GabrielBarrosoBuenoUCM <dreaygd9@gmail.com>
2025-04-14 15:24:50 +0200

    Setting up tileset (no context on tile drawing)

--- KleptoMagic_project/src/map/Tilemap.cpp
+++ KleptoMagic_project/src/map/Tilemap.cpp
@@ -9,6 +9,12 @@ Tilemap::Tilemap(vector<vector<char>> tilematrix, DungeonRoom* dungeonroom) : ro
 	// Instantiate the tilemap with the same size as the tile matrix
 	tilemap = vector<vector<TileType>>(tilematrix.size(), vector<TileType>(tilematrix[0].size(), TILE_BLANK));
 
+	// Prepare the tileset texture
+	tileset = &sdlutils().images().at("tileset");
+	tilesetTileSize = 32;
+	tilesetWidth = 5;
+	tilesetHeight = 5;
+
 	// Run through the matrix and translate chars to tile types within the tilemap itself
 	for (int i = 0; i < tilematrix.size(); i++) {
 		for (int j = 0; j < tilematrix[0].size(); j++) {
@@ -63,6 +69,10 @@ Tilemap::Tilemap(vector<vector<char>> tilematrix, DungeonRoom* dungeonroom) : ro
 }
 
 void Tilemap::render(SDL_Renderer* renderer) {
+	render_advanced();
+}
+
+void Tilemap::render_basic(SDL_Renderer* renderer) {
 	for (int x = 0; x < getTilemapWidth(); x++) {
 		for (int y = 0; y < getTilemapHeight(); y++) {
 			SDL_Rect tileRect;
@@ -73,25 +83,74 @@ void Tilemap::render(SDL_Renderer* renderer) {
 
 			switch (tilemap[x][y]) {
 			case TILE_BLANK:
-				SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xFF); // replace later with actual texture
+				SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xFF);
 				break;
 			case TILE_FLOOR:
-				SDL_SetRenderDrawColor(renderer, 0x00, 0xFF, 0x00, 0xFF); // replace later with actual texture
+				SDL_SetRenderDrawColor(renderer, 0x00, 0xFF, 0x00, 0xFF);
 				break;
 			case TILE_WALL:
-				SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0xFF); // replace later with actual texture
+				SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0xFF);
 				break;
 			case TILE_HOLE:
-				SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0xFF, 0xFF); // replace later with actual texture
+				SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0xFF, 0xFF);
 				break;
 			default:
-				SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF); // replace later with actual texture
+				SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
 				break;
 			}
 
 			SDL_RenderDrawRect(renderer, &tileRect);
 		}
 	}
+}
+
+void Tilemap::render_advanced() {
+	for (int x = 0; x < getTilemapWidth(); x++) {
+		for (int y = 0; y < getTilemapHeight(); y++) {
+			SDL_Rect tileRect;
+			tileRect.w = TILE_SIZE;
+			tileRect.h = TILE_SIZE;
+			tileRect.x = (x * TILE_SIZE) + xOffset;
+			tileRect.y = (y * TILE_SIZE) + yOffset;
+			
+			render_tile_with_context(x, y, tileRect);
+		}
+	}
+}
+
+void Tilemap::render_tile_with_context(int x, int y, SDL_Rect dstRect) {
+
+	double angle = 0;
+	int srcX = 0;
+	int srcY = 0;
+
+	switch (tilemap[x][y]) {
+	case TILE_BLANK:
+		srcX = 0;
+		srcY = 0;
+		break;
+	case TILE_FLOOR:
+		srcX = 0;
+		srcY = 1;
+		break;
+	case TILE_WALL:
+		srcX = 1;
+		srcY = 0;
+		break;
+	case TILE_HOLE:
+		srcX = 0;
+		srcY = 0;
+		break;
+	default:
+		break;
+	}
+	SDL_Rect srcRect;
+	srcRect.w = tilesetTileSize;
+	srcRect.h = tilesetTileSize;
+	srcRect.x = (srcX * tilesetTileSize);
+	srcRect.y = (srcY * tilesetTileSize);
+	
+	tileset->render(srcRect, dstRect, angle);
 }
 
 int Tilemap::checkCollision(int x, int y) {
