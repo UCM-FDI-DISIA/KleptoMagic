819a6156942aa5484fe82b29ea0cbcb2d789bfb3
Mauro Martínez <maumar03@ucm.es>
2025-06-28 15:06:32 +0200

    merge choto arreglado

--- KleptoMagic_project/src/state/RunningState.cpp
+++ KleptoMagic_project/src/state/RunningState.cpp
@@ -320,7 +320,7 @@ void RunningState::checkCollisions() {
 		}
 
 	}
-	for (auto* upgrade : game().getMngr()->getEntities(ecs::grp::UPGRRADE)) 
+	for (auto* upgrade : game().getMngr()->getEntities(ecs::grp::OBJECT))
 	{
 		if(game().getMngr()->isAlive(upgrade))
 		{
